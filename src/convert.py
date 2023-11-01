66a93ec11f3cb498fe4c4a442c66e97819cb3b28
erastov-alex <erastov.alex@gmail.com>
2023-11-01 12:56:55 +0300

    upd convert, settings, gitignore

--- src/convert.py
+++ src/convert.py
@@ -3,8 +3,8 @@ import os
 from dataset_tools.convert import unpack_if_archive
 import src.settings as s
 from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
-import shutil
+from supervisely.io.fs import get_file_name, file_exists, dir_exists
+import xml.etree.ElementTree as ET
 
 from tqdm import tqdm
 
@@ -69,17 +69,76 @@ def count_files(path, extension):
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    train_path = os.path.join("archive","training_data","training_data")
+    test_path = os.path.join("archive","test_data_images","test_data_images")
+    batch_size = 30
+    ds_name = "ds"
+    images_folder = "images"
+    bboxes_folder = "labels"
+    images_ext = ".jpg"
+    ann_ext = ".xml"
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
-
-    # ... some code here ...
-
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
-
-    # return project
+    ds_name_to_data = {"train": train_path, "test": test_path}
 
 
+    def create_ann(image_path):
+        labels = []
+
+        file_name = get_file_name(image_path)
+
+        ann_path = os.path.join(bboxes_path, file_name + ann_ext)
+        if file_exists(ann_path):
+            tree = ET.parse(ann_path)
+            root = tree.getroot()
+
+            img_height = int(root.find(".//height").text)
+            img_wight = int(root.find(".//width").text)
+
+            ann_objects = root.findall(".//object")
+            for curr_object in ann_objects:
+                obj_class_name = curr_object[0].text
+                obj_class = name_to_class.get(obj_class_name)
+                left = float(curr_object[1][0].text)
+                top = float(curr_object[1][1].text)
+                right = float(curr_object[1][2].text)
+                bottom = float(curr_object[1][3].text)
+
+                rect = sly.Rectangle(left=left, top=top, right=right, bottom=bottom)
+                label = sly.Label(rect, obj_class)
+                labels.append(label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+
+    car = sly.ObjClass("car", sly.Rectangle)
+    pool = sly.ObjClass("pool", sly.Rectangle)
+    name_to_class = {"1": car, "2": pool}
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=[car, pool])
+    api.project.update_meta(project.id, meta.to_json())
+
+    for ds_name, ds_path in ds_name_to_data.items():
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+        images_path = os.path.join(ds_path, images_folder)
+        bboxes_path = os.path.join(ds_path, bboxes_folder)
+
+        images_names = os.listdir(images_path)
+
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+        for images_names_batch in sly.batched(images_names, batch_size=batch_size):
+            img_pathes_batch = [
+                os.path.join(images_path, image_name) for image_name in images_names_batch
+            ]
+
+            img_infos = api.image.upload_paths(dataset.id, images_names_batch, img_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+
+            if dir_exists(bboxes_path):
+                anns = [create_ann(image_path) for image_path in img_pathes_batch]
+                api.annotation.upload_anns(img_ids, anns)
+
+            progress.iters_done_report(len(images_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,25 +13,25 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Swimming Pool And Car Detection"
+PROJECT_NAME_FULL: str = "Swimming Pool And Car Detection"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.GNU_GPL_v2()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Domain.Geospatial(is_used=False)]
+CATEGORY: Category = Category.Satellite()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [CVTask.ObjectDetection()]
 
 RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
-    RELEASE_YEAR: int = None
+    RELEASE_YEAR: int = 2018
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/kbhartiya83/swimming-pool-and-car-detection"
 # e.g. "https://some.com/dataset/homepage"
 
 PREVIEW_IMAGE_ID: int = None
@@ -43,7 +43,7 @@ GITHUB_URL: str = None
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = ["https://www.kaggle.com/datasets/kbhartiya83/swimming-pool-and-car-detection"]
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -53,10 +53,10 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # Use dict key to specify name for a button
 PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"some_link_to_repo_if_exists"}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = ["Kartik Bhartiya"]
 AUTHORS_CONTACTS: Optional[List[str]] = None
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
